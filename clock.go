299a69ef15c0abfa9a3448b2f8eda1ad32f0d8c0
Christian Hujer <Christian.Hujer@nelkinda.com>
2019-02-26 09:01:10 +0530

    Add documentation.

--- clock.go
+++ clock.go
@@ -2,10 +2,12 @@ package clock
 
 import "time"
 
+// Clock provides access to the current instant, date and time, optionally using a time-zone.
 type Clock interface {
 	Now() time.Time
 }
 
+// Tardis is a special type of clock that can travel by telling which other clock it shows.
 type Tardis struct {
 	Clock
 	clock Clock
@@ -38,27 +40,33 @@ func (s wallClock) Now() time.Time {
 
 func (t Tardis) Now() time.Time {
 	if t.clock == nil {
-		return time.Now()
+		return time.Now().UTC()
 	}
 	return t.clock.Now()
 }
 
+// Fixed creates a clock that will always return the same time at the same location.
 func Fixed(t time.Time, z *time.Location) Clock {
 	return &fixedClock{z: z, t: t}
 }
 
+// System creates a clock that will always return the system time at the specified location.
 func System(z *time.Location) Clock {
 	return &wallClock{z: z}
 }
 
+// SystemUTC creates a clock that will always return the system time in UTC.
 func SystemUTC() Clock {
 	return &wallClock{z: time.UTC}
 }
 
+// SystemDefaultZone creates a clock that will always return the system time in the system's default zone.
 func SystemDefaultZone() Clock {
 	return &wallClock{}
 }
 
+// NewTardis creates a clock that will return the time of whatever Clock is used as reference of the Tardis.
+// The clock can be changed, and if it is nil, the Tardis will use time.Now().UTC().
 func NewTardis(initialClock Clock) Tardis {
 	return Tardis{clock: initialClock}
 }
